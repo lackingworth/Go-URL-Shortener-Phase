ac3e81450955a40cfd6ca3233d995a6f08e043c8
lackingworth <worstface@yandex.ru>
2023-10-01 13:57:10 +0300

    Init

---
+++ api/routes/resolve.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"github.com/lackingworth/Go-URL-Shortener/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/go-redis/redis/v8"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+	value, err := r.Get(database.Ctx, url).Result()
+
+	if err != redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Short not found in the database"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"Cannot connect to database"})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
